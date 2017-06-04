e756da347a0f61ca4c9303cc9672289afc10ef4d
huangleo123@hotmail.com <a159753>
2017-06-04 21:28:32 +0800

    简单提交

--- app/src/main/java/com/united/smartpots/MainActivity.java
+++ app/src/main/java/com/united/smartpots/MainActivity.java
@@ -2,8 +2,13 @@ package com.united.smartpots;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView iv_home;
+    private ImageView iv_social;
+    private ImageView iv_talk;
+    private ImageView iv_mormal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
