3ab64f475595dc7e1e7026e5aaa6aaad9f6f050e
huangleo123@hotmail.com <a159753>
2017-06-04 23:19:42 +0800

    用来别人的轮子来实现的底部导航栏

--- app/src/main/java/com/united/smartpots/MainActivity.java
+++ app/src/main/java/com/united/smartpots/MainActivity.java
@@ -1,8 +1,15 @@
 package com.united.smartpots;
 
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+import com.aurelhubert.ahbottomnavigation.notification.AHNotification;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView iv_home;
@@ -14,5 +21,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AHBottomNavigation bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem("首页", R.mipmap.profile_ic_home_normal, R.color.colorAccent);//花盆
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem("", R.mipmap.profile_ic_social_normal, R.color.base);//话题
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem("", R.mipmap.profile_ic_talk_normal, R.color.base);//圈子
+        AHBottomNavigationItem item4 = new AHBottomNavigationItem("", R.mipmap.profile_ic_talk_normal, R.color.base);//更多
+
+        bottomNavigation.addItem(item1);
+        bottomNavigation.addItem(item2);
+        bottomNavigation.addItem(item3);
+        //设置背景颜色
+        bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#EDEBDE"));
+
+        // Disable the translation inside the CoordinatorLayout（可能是动画效果）
+        bottomNavigation.setBehaviorTranslationEnabled(false);
+
+        bottomNavigation.setAccentColor(Color.parseColor("#FF4081"));
+        bottomNavigation.setInactiveColor(Color.parseColor("#3F51B5"));
+        //使用彩色导航与圆显示效果
+        bottomNavigation.setColored(false);
+
+        //强制绘制（可用于例如图标的字体）
+        bottomNavigation.setForceTint(true);
+
+
+        //管理标题
+        bottomNavigation.setTitleState(AHBottomNavigation.TitleState.SHOW_WHEN_ACTIVE);
+
+        // Add or remove notification for each item
+        bottomNavigation.setNotification("", 0);//加了小红点
+
+        //设置小红点的样式，然后添加
+        /*AHNotification notification = new AHNotification.Builder()
+                .setText("2")//个数
+                .setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.red))
+                .setTextColor(ContextCompat.getColor(MainActivity.this, R.color.base))
+                .build();
+        bottomNavigation.setNotification(notification, 2);*/
+
+        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+            @Override
+            public boolean onTabSelected(int position, boolean wasSelected) {
+                // Do something cool here...
+                if (position==0){
+                    Toast.makeText(getApplicationContext(),"点击了第一个",Toast.LENGTH_LONG).show();
+                }
+                if (position==1){
+                    Toast.makeText(getApplicationContext(),"点击了第er个",Toast.LENGTH_LONG).show();
+                }
+                if (position==2){
+                    Toast.makeText(getApplicationContext(),"点击了第san个",Toast.LENGTH_LONG).show();
+                }
+                return true;
+            }
+        });
+
     }
 }
